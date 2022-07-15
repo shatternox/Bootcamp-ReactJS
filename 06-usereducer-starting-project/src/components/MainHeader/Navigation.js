ede20e603c674637b4ccd5f5e6e948bf79b7d2b8
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2022-07-15 11:42:43 +0700

    .
    .

--- 06-usereducer-starting-project/src/components/MainHeader/Navigation.js
+++ 06-usereducer-starting-project/src/components/MainHeader/Navigation.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, {useContext} from "react";
+import AuthContext from "../../store/auth-context";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+
+  const context = useContext(AuthContext);
+
   return (
+    // Provider yang ngambil, dan Consumer yang gunain
+
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {context.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {context.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {context.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
